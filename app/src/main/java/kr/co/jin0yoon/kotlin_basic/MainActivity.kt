73b93ba537391e9a289387dab07064af892d8067
Yoon Jinyeong <wwwz4805@naver.com>
2020-06-14 17:05:07 +0900

    Update MainActivity.kt

--- app/src/main/java/kr/co/jin0yoon/kotlin_basic/MainActivity.kt
+++ app/src/main/java/kr/co/jin0yoon/kotlin_basic/MainActivity.kt
@@ -4,6 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+/*
+kotlin을 시작하기 전에 알아야할 지식
+- 종결자를 선언하지 않아도 된다.
+- 형을 선언하지 않았다면 변수를 대입시 형이 결정된다.
+- 변수는 읽기전용과 읽기쓰기 상태로 선언된다.
+- null은 엄격히 구분한다. -> null을 허용하는 변수는 ?로 정의
+- kt 파일은 내부적으로 Class로 재구성한다.
+ */
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
