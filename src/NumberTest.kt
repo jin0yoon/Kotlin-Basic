449c9edd9ecd4321e26aeab67326a93e8980213e
Yoon Jinyeong <wwwz4805@naver.com>
2020-06-17 20:00:16 +0900

    Create NumberTest.kt

---
+++ src/NumberTest.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    //숫자형은 데이터 형을 지정해주는 것이 좋음
+
+    var doubleV : Double = 110.1111
+    var floatV : Float = 110.1f
+    var longV : Long = 110
+    var intV : Int = 20
+    var shortV : Short = 30
+    var byteV : Byte = 10
+
+    println(doubleV)
+    println(floatV)
+    println(intV)
+    
+}
