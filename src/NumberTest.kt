8ace0d65eabaac6e526eaf4c888eeb38d7adc8c4
Yoon Jinyeong <wwwz4805@naver.com>
2020-06-18 11:45:59 +0900

    Update NumberTest.kt

--- src/NumberTest.kt
+++ src/NumberTest.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
 
     //숫자형은 데이터 형을 지정해주는 것이 좋음
 
+    //숫자형 DataType 크기 순으로 선언
     var doubleV : Double = 110.1111
     var floatV : Float = 110.1f
     var longV : Long = 110
@@ -9,8 +10,22 @@ fun main(args: Array<String>) {
     var shortV : Short = 30
     var byteV : Byte = 10
 
+    //출력해보기
     println(doubleV)
     println(floatV)
     println(intV)
     
+    //크기 변환 후, 대입 : 캐스팅
+    //to대입할크기() 메소드를 사용한다.
+    //** as로 형변환은 기본형에서는 안된다. **
+    doubleV = intV.toDouble()
+
+    //회색인 이유는?
+    intV = doubleV.toInt()
+
+    //문자열로 변환
+    println(byteV.toString())
+
+    //문자를 숫자로 변환
+    val sum = "123".toLong() + 10
 }
