c33216197cbd1e9aafbf863a8a689eb3fc30b4b1
Yoon Jinyeong <wwwz4805@naver.com>
2020-06-26 13:55:32 +0900

    Update LabelTest.kt

--- src/LabelTest.kt
+++ src/LabelTest.kt
@@ -6,3 +6,32 @@ Kotlin에서 레이블은 매우 중요하다.
 - break, continue : break는 반복문을 종료하고 나간다. continue는 다음 라인을 무시하고 반복문의 처음으로 간다.
 - break@레이블, continue@레이블 : break는 반복문을 종료하고 지정한 레이블로 나간다. continue는 다음라인을 무시하고 지정한 레이블로 이동한다.
  */
+
+fun main(args : Array<String>) {
+    ExitLoop()
+    println(lambdaReturn())
+
+    //함수형 스타일의 활용
+    (0..10).forEach{if (it > 3) return@forEach else println(it)}
+}
+
+//@레이블 지정 후, break@레이블로 나감
+//대부분의 언어에서 지원하는 goto문과 유사함.
+fun ExitLoop(){
+    HereToExit@ for (i in 0..2){
+        for (j in 0..10){
+            if (j == 5) break@HereToExit
+            println("i->$i, j->$j")
+        }
+        println("j loop end")
+    }
+    println("i loop end")
+}
+
+//이름없는 함수(람다식)의 리턴
+var lambdaReturn = Exit@{
+    if (true){
+        return@Exit 3
+    }
+    1000
+}
