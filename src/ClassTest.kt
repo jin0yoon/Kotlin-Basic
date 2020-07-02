f88a213571964df617fe0ac0833686ef388183c6
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-02 13:39:18 +0900

    Update ClassTest.kt

--- src/ClassTest.kt
+++ src/ClassTest.kt
@@ -27,3 +27,23 @@ class Test1{
 
     fun showInfo() = println("${sName} : ${nAge}")
 }
+
+//생성자를 ()로 처리하는 클래스
+//클래스에서 다른 생성자를 정의하려면
+//constructor 뒤에 : this(정의한 값)을 덛붙힌다.
+class Test2(name: String){
+    var sName : String = "Test2"
+    var nAge : Int = 10
+
+    init {
+        sName = name
+    }
+
+    constructor(name: String, age: Int):
+            this(name){
+        sName = name
+        nAge = age
+    }
+
+    fun showInfo() = println("${sName} : ${nAge}")
+}
