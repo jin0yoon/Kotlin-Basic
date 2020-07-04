05950ffc316e34764cf9f8576b88a3d7fe805b78
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-04 21:09:30 +0900

    Create Test.kt

---
+++ src/Test.kt
@@ -0,0 +1,6 @@
+/*
+interface, abstract, static 구현하는 방법
+- interface는 interface 이름 {}으로 정의한다. 오버라이드하기 위해 open이나 기타 지시자로 정의할 필요가 없다.
+- abstract class는 구현상속할 메소드도 abstract로 정의해야 한다. 그리고 상속에서는 override로 정의한다.
+- static을 사용하려면 클래스 내에 companion object {}를 만들고 그 안에 메소드나 멤버필드를 정의한다.
+ */
