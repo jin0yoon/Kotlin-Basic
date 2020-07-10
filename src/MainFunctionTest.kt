1368bc083fcd535b7e2a13aad9c6f089dae00fa5
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-10 18:15:53 +0900

    Update MainFunctionTest.kt

--- src/MainFunctionTest.kt
+++ src/MainFunctionTest.kt
@@ -19,13 +19,29 @@ fun main(args : Array<String>) {
     bug()?.let { println(it) }
 
     //2. apply()
-    
+    var obj = Test().apply{sName = "박모씨"; nAge = 49;}
+    obj.aboutMe()
 
     //3. run()
+    obj.run { sName = "김모씨"; aging(); sName }.let { println(it) }
+    obj.aboutMe()
 
     //실행결과를 리턴
+    run { 333 + 4 }.let { println(it) }
 
     //4. also()
+    100.let { println(it); it }
+        .also { println(it + 1000) }
+        .also { println(it + 20) }
+        .let { println(it) }
 }
 
 fun f1() = 10
+
+class Test{
+    var sName : String = "무명씨"
+    var nAge : Int = 49
+
+    fun aboutMe() = println("이름은 \"$sName\"이고 나이는 $nAge 입니다.")
+    fun aging() = nAge++
+}
