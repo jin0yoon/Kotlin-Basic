50b8b73df19c45ee665134f1ce59001834a0cdb3
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-14 11:50:23 +0900

    Update InfixTest.kt

--- src/InfixTest.kt
+++ src/InfixTest.kt
@@ -4,3 +4,32 @@ infix
 - .연산자를 대처하는 메소드 표기법
 - 좌측은 객체 우측은 1개의 값
  */
+
+fun main(args : Array<String>) {
+    //삶은 이런것!
+    var MyCompany = RankMyCompany()
+    MyCompany AddPoint "일이좋다"
+    MyCompany AddPoint "자부심"
+    MyCompany AddPoint "높은연봉"
+    MyCompany AddPoint "유명회사"
+    MyCompany AddPoint "야근당연"
+
+    println(MyCompany.Rank)
+}
+
+class RankMyCompany{
+    val m = mapOf<String, Int>(
+        "일이좋다"  to  25,
+        "자부심"  to  30,
+        "높은연봉"  to  25,
+        "유명회사"  to  20,
+        "야근당연"  to  -60)
+
+    var Rank:Int = 0
+
+    //infix는 확장함수나 멤버함수로 구현되어야 한다.
+    infix fun AddPoint(s:String) : Int{
+        this.Rank += m[s]!!
+        return this.Rank
+    }
+}
