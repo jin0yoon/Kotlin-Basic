3eef60f4607caee3bf395f327730eb69ae0b9fbb
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-16 21:08:00 +0900

    Update LazyTest.kt

--- src/LazyTest.kt
+++ src/LazyTest.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 /*
 late, lazy 초기화
 - [lateinit]
@@ -10,3 +12,59 @@ late, lazy 초기화
 - primitive형도 가능함.
 - 초기화 실행 시, 일련의 코딩도 가능하다.
  */
+
+fun main(args : Array<String>) {
+    //늦은 초기화의 필요성 (메모리 효율, 에러 방지 차원)
+    //lateinit과 lazy
+    var l : LateInitTest = LateInitTest()
+    l.Test()
+
+    var l2 : LazyInitTest = LazyInitTest()
+    l2.Test()
+}
+
+//kotlin에서는 java와 달리 멤버필드를 반드시 초기화 해야한다.
+//아니면 abstract나 lateinit으로 선언해주어야 한다.
+
+//1. var만 사용가능.
+//2. 늦은 초기화이므로 초기화전에 사용불가
+//3. primitive형은 불가능함
+
+class LateInitTest{
+    var normalValue:Int = 3
+    var initTest:Int
+
+    //"선언 = 초기화"를 안해도 됨.
+    lateinit var late:String
+
+    init {
+        initTest = 0
+    }
+
+    fun Test(){
+        this.late = "lateinit 초기화"
+        println(this.late)
+    }
+}
+
+//1. val로 선언
+//2. 사용하는 시점에서 초기화가 1회 발생
+//3. prinitive형도 가능
+
+class LazyInitTest{
+    val lazy1:String by lazy {
+        "lazyinit 초기화"
+    }
+
+    val lazy2:Int by lazy {
+        println("1번만 실행")
+        //값
+        Random().nextInt(100)
+    }
+
+    fun Test(){
+        println(lazy1)
+        println(lazy2)
+        println(lazy2)
+    }
+}
