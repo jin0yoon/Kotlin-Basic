6bd8630bbc20b69102cfe63f6b9bb1d832dea136
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-17 21:32:09 +0900

    Create JavaActivity.java

---
+++ app/src/main/java/kr/co/jin0yoon/kotlin_basic/javainterlop/JavaActivity.java
@@ -0,0 +1,15 @@
+/*
+java interlop
+- kotlin에서 java를 사용하는 것은 너무 자연스럽다.
+- java에서 kotlin을 사용하면
+- 파일명(첫번째 대문자)Kt 형식으로 class가 생성된다.
+- 파일 안에 정의된 일반 변수(클래스 포함)는 get/set 형식의 메소드로 사용 가능하다.
+    - java에서 메소드 형식으로 변환없이 사용하려면 @JvmField 어노테이션을 사용한다.
+    - java에서 스태틱 메소드를 사용하려면 @JvmStatic 어노테이션을 사용한다.
+- 확장함수를 사용하면 java 소스 내용을 공유하여 부분적으로 코틀린으로 코딩할 수 있다.
+ */
+
+package kr.co.jin0yoon.kotlin_basic.javainterlop;
+
+public class JavaActivity {
+}
