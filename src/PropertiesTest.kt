ed450c17f83e2f05ea48ec1a2459cc916671ad5e
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-17 14:49:13 +0900

    Update PropertiesTest.kt

--- src/PropertiesTest.kt
+++ src/PropertiesTest.kt
@@ -5,3 +5,21 @@ properties
 - get(), set() 함수로 읽고 쓰기를 관리한다.
 - 자기자신을 가리키는 예약어는 field이다.
  */
+
+fun main(args: Array<String>) {
+    //1. 변수를 가져오거나 대입할 때, 함수처럼 프로그래밍이 가능한 것
+    passwd = "123456787657572313"
+    println(passwd)
+}
+
+var passwd:String = ""
+    get() {
+        if (field.length > 5){
+            field = "길이를 초과했음"
+        }
+        return field
+    }
+    set(s:String) {
+        println("\"${s}\"을 저장함")
+        field = s
+    }
