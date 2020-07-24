09ae64df390ecff87c03f1393395f2f0edec13c9
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-24 16:13:50 +0900

    Update MultiReturnTest.kt

--- src/MultiReturnTest.kt
+++ src/MultiReturnTest.kt
@@ -3,7 +3,7 @@ multi return (destructuring)
 - 우측의 값을 좌측의 변수로 나누어 순서대로 대입하는 것
 - ()안에 변수를 나열한다. (일반적으로 Data class, List, Pair Collection의 반환값)
 - 순서대로 특정범위까지 가져올 수 있다.
-- paation 메소드 사용법
+- partion 메소드 사용법
  */
 
 fun main(args : Array<String>) {
