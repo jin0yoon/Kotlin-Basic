1f64cf0db7fa41d6945cc4555290f375d66be715
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-24 16:13:36 +0900

    Update TestClass.kt

--- app/src/main/java/kr/co/jin0yoon/kotlin_basic/TestClass.kt
+++ app/src/main/java/kr/co/jin0yoon/kotlin_basic/TestClass.kt
@@ -1,5 +1,8 @@
 package kr.co.jin0yoon.kotlin_basic
 
+//TestClass는 우리가 안드로이드 화면에서 인텔리제이에서 만든 예제들을 println했던 것을 똑같이 화면에서 보여주기 위해서 만든 클래스
+//인텔리제이에서 만든 예제들을 TestClass를 상속받아서 doTest라는 메소드에서 구현할 것임
+
 open class TestClass(pFunc : (Any) -> Unit){
 
     // TestClass를 상속받았거나 그 안의 innerClass에서
